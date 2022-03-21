1dc2f94753247d689466ef9ade8fdb7619e04eba
dabdubidab <rad.dab.gitlab@gmail.com>
2022-03-21 09:25:04 +0100

    Creating project Creating channels and routing

---
+++ talkative/talkative/asgi.py
@@ -0,0 +1,32 @@
+"""
+ASGI config for talkative project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkative.settings')
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat import routing
+
+
+
+application = ProtocolTypeRouter({
+  "http": django_asgi_app,
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+           routing.websocket_urlpatterns
+        )
+    ),
+})
+
+
